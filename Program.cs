14929ae13b281bc14a22dcc77520e1137759ee9d
Mariana Lopez <mariana.lopez.it@gmail.com>
2022-11-29 22:50:34 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace TurnosMedicos
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Menu menu = new Menu(); /* Creo una instancia de Menu */
+			menu.Principal(); /* Invoco al método Principal definido en la clase Menu */
+		}
+	}
+}
